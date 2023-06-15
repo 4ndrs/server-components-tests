d6a0d986e4f7c6ca201494ef9ab32d6d8fb43fc5
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-15 22:00:30 +0200

    Add cookies test

--- app/test2/page.tsx
+++ app/test2/page.tsx
@@ -1,14 +1,25 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 const Page = () => {
   const headersList = headers();
+  const cookieStore = cookies();
 
   return (
     <>
+      <h1 className="mb-2 text-xl font-bold">Headers</h1>
       <ul>
         {Array.from(headersList.entries()).map(([key, value]) => (
           <li key={key}>
             <strong>{key}:</strong> {value || "{BLANK}"}
+          </li>
+        ))}
+      </ul>
+
+      <h1 className="mb-2 mt-6 text-xl font-bold">Cookies</h1>
+      <ul>
+        {cookieStore.getAll().map((cookie) => (
+          <li key={cookie.name}>
+            <strong>{cookie.name}:</strong> {cookie.value}
           </li>
         ))}
       </ul>
