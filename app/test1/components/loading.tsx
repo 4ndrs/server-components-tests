721ad3a4029cc699d022aba87819008840d1ea3d
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-15 11:06:26 +0200

    Add suspense test with loading ui

---
+++ app/test1/components/loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => (
+  <div className="mt-1 flex flex-col gap-1">
+    <div className="h-5 w-12 animate-pulse bg-slate-300" />
+    <div className="h-5 w-64 animate-pulse bg-slate-300" />
+  </div>
+);
+
+export default Loading;
--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
+
 import AsyncFetch from "./components/async-fetch";
+import Loading from "./components/loading";
 
 const Test1 = () => {
   return (
-    <>
+    <Suspense fallback={<Loading />}>
       <AsyncFetch />
-    </>
+    </Suspense>
   );
 };
 
